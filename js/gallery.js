5e1f55993dcc3c801734fcf5dd381dee95acd9eb
Bandura Yulia <yuliabandura@gmail.com>
2021-02-01 12:59:08 +0200

    чистка коду

--- js/gallery.js
+++ js/gallery.js
@@ -3,42 +3,30 @@ import * as galleryItems from "./gallery-items.js";
 
 const galleryList = galleryItems.default;
 
-const galleryRef = document.querySelector(".js-gallery");
+const refs = {
+  galleryRef: document.querySelector(".js-gallery"),
+  lightBoxRef: document.querySelector(".js-lightbox"),
+  lightBoxOverlayRef: document.querySelector(".lightbox__overlay"),
+  lightBoxImageRef: document.querySelector(".lightbox__image"),
+  closeLightBoxBtn: document.querySelector(
+    'button[data-action="close-lightbox"]'
+  ),
+};
 
-// const getGalleryList = galleryList.map((galleryList) => {
-// const liRef = document.createElement("li");
-// liRef.classList.add("gallery__item");
-// const aRef = document.createElement("a");
-// aRef.classList.add("gallery__link");
-// aRef.href = galleryList.original;
-// const imgRef = document.createElement("img");
-// imgRef.classList.add("gallery__image");
-// imgRef.src = galleryList.preview;
-// imgRef.dataset.source = galleryList.original;
-// imgRef.alt = galleryList.description;
-// liRef.append(aRef);
-// aRef.append(imgRef);
-// return liRef;
-// });
 const getGalleryList = galleryList
   .map((galleryList) => {
-    return ` <li class='gallery__item'><a class='gallery__link' href='${galleryList.original}'><img class='gallery__image' alt='${galleryList.description}' src="${galleryList.preview}" data-source="${galleryList.original}"></a></li > `;
+    return ` <li class='gallery__item'>
+    <a class='gallery__link' href='${galleryList.original}'>
+    <img class='gallery__image' alt='${galleryList.description}' src="${galleryList.preview}" data-source="${galleryList.original}">
+    </a></li > `;
   })
   .join("");
-galleryRef.insertAdjacentHTML("afterbegin", getGalleryList);
 
-// galleryRef.append(...getGalleryList);
+refs.galleryRef.insertAdjacentHTML("afterbegin", getGalleryList);
 
-const lightBoxRef = document.querySelector(".js-lightbox");
-const lightBoxOverlayRef = document.querySelector(".lightbox__overlay");
-const modalImageRef = document.querySelector(".lightbox__image");
-const closelightBoxBtn = document.querySelector(
-  'button[data-action="close-lightbox"]'
-);
+refs.galleryRef.addEventListener("click", onImageClick);
 
-galleryRef.addEventListener("click", onTagsClick);
-
-function onTagsClick(event) {
+function onImageClick(event) {
   event.preventDefault();
 
   const imagesRef = event.target;
@@ -47,19 +35,19 @@ function onTagsClick(event) {
   if (imagesRef.nodeName !== "IMG") {
     return;
   }
-  lightBoxRef.classList.add("is-open");
-  modalImageRef.setAttribute("src", largeImgUrl);
+  refs.lightBoxRef.classList.add("is-open");
+  refs.lightBoxImageRef.setAttribute("src", largeImgUrl);
   window.addEventListener("keydown", closeLightBoxByEsc);
-  lightBoxOverlayRef.addEventListener("click", closelightBoxByClick);
-  closelightBoxBtn.addEventListener("click", closeLightBoxByBtn);
+  refs.lightBoxOverlayRef.addEventListener("click", closelightBoxByClick);
+  refs.closeLightBoxBtn.addEventListener("click", closeLightBoxByBtn);
 }
 
 function closeLightBoxByBtn() {
-  lightBoxRef.classList.remove("is-open");
-  modalImageRef.setAttribute("src", "");
+  refs.lightBoxRef.classList.remove("is-open");
+  refs.lightBoxImageRef.setAttribute("src", "");
   window.removeEventListener("keydown", closeLightBoxByEsc);
-  lightBoxOverlayRef.removeEventListener("click", closelightBoxByClick);
-  closelightBoxBtn.removeEventListener("click", closeLightBoxByBtn);
+  refs.lightBoxOverlayRef.removeEventListener("click", closelightBoxByClick);
+  refs.closeLightBoxBtn.removeEventListener("click", closeLightBoxByBtn);
 }
 
 function closeLightBoxByEsc(event) {
